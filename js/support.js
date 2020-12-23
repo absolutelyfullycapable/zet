ca464fc1962a93b899717c17d51106a28b6a4ee5
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-12-23 17:40:58 +0900

    modify support HTML, CSS, JS/jQuery

--- js/support.js
+++ js/support.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
 
     $('a[href="#"]').click(function(e){
-                        e.preventDefault();
-                       });
+        e.preventDefault();
+    });
 
-        //nav 버튼 변경, 배경색
+    //nav 버튼 변경, 배경색
     $('.btn-menu').click(function(){
         $('.btn-menu').css('display','none');
         $('.btn-menu-close').css('display','block');
         $('.nav_bg').css('display','block');
-        $('body').addClass('lock');
+        $('html').css('overflow', 'hidden');
     });
     $('.btn-menu-close').click(function(){
         $('.btn-menu-close').css('display','none');
         $('.btn-menu').css('display','block');
         $('.nav_bg').css('display','none');
-        $('body').removeClass('lock');
+        $('html').css('overflow', 'initial');
     });
 
     //accodian menu
-    $('.faq-board').find('a').on('click',function(){
-        var inx= $(this).parents('li').index();
+    $('.faq-board a').on('click',function(){
+        var inx = $(this).parents('li').index();
+
         $(this).parents('li').addClass('on').siblings().removeClass('on');
         if($(this).parents('li').is(':visible')){
             $(this).next().slideUp();
