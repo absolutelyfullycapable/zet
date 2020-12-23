b75e22a4eaf4207ef2b6baaf64dc5c09f4f0f80e
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-12-23 17:41:22 +0900

    modify index CSS, JS/jQuery

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
   $(document).ready(function(){
 
     $('a[href="#"]').click(function(e){
-                        e.preventDefault();
-                       });
+        e.preventDefault();
+    });
 
     //nav 버튼 변경, 배경색
     $('.btn-menu').click(function(){
         $('.btn-menu').css('display','none');
         $('.btn-menu-close').css('display','block');
         $('.nav_bg').css('display','block');
+        $('html').css('overflow', 'hidden');
     });
     $('.btn-menu-close').click(function(){
         $('.btn-menu-close').css('display','none');
         $('.btn-menu').css('display','block');
-        $('.nav_bg').css('display','none'); 
+        $('.nav_bg').css('display','none');
+        $('html').css('overflow', 'initial');
     });
 
 
